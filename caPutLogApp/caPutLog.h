894a322f3eb55ff91331645b5814e5d1c1a1302b
Ernest Williams <ernesto@slac.stanford.edu>
2009-01-29 23:35:29 +0000

    add caPutLog module to log operator and/or application channel access puts

---
+++ caPutLogApp/caPutLog.h
@@ -0,0 +1,27 @@
+#ifndef INCcaPutLogh
+#define INCcaPutLogh 1
+
+#include <shareLib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* status return values */
+#define caPutLogSuccess     0
+#define caPutLogError       -1
+
+/* for parameter 'config' of caPutLogInit and caPutLogReconfigure */
+#define caPutLogNone        -1  /* no logging (disable) */
+#define caPutLogOnChange    0   /* log only on value change */
+#define caPutLogAll         1   /* log all puts */
+
+epicsShareFunc int epicsShareAPI caPutLogInit (const char *addr_str, int config);
+epicsShareFunc int epicsShareAPI caPutLogReconf (int config);
+epicsShareFunc void epicsShareAPI caPutLogShow (int level);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*INCcaPutLogh*/
